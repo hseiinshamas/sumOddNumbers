e711f7cc457fe124aa30f25bae39cfa2b98b0c60
Hussein Shamas <73178084+hssen139@users.noreply.github.com>
2022-06-13 15:04:21 +0300

    Update CodeWars.java

--- src/CodeWars.java
+++ src/CodeWars.java
@@ -1,2 +1,26 @@
+/*Description:
+
+        Given the triangle of consecutive odd numbers:
+
+        1
+        3     5
+        7     9    11
+        13    15    17    19
+        21    23    25    27    29
+        ...
+ */
+
 package PACKAGE_NAME;public class CdoeWars {
+ public static int rowSumOddNumbers(int n) {
+
+        return n*n*n;
+    }
+    public static void main(String[] args) {
+        System.out.println(rowSumOddNumbers(2)); // output :  3 + 5 = 8
+        }
+    }
+
+
+
+
 }
